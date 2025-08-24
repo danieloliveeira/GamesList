cd3f56240d766ed4a98480bd6d34e4f4a053123d
danieloliveeira <danieldoskil@gmail.com>
2025-08-24 12:08:13 -0300

    feat(service): Cria um novo metodo que busca por ID

--- src/main/java/com/wydengames/wydenlist/services/GameService.java
+++ src/main/java/com/wydengames/wydenlist/services/GameService.java
@@ -1,9 +1,13 @@
 package com.wydengames.wydenlist.services;
 
+import com.wydengames.wydenlist.dto.GameDTO;
 import com.wydengames.wydenlist.dto.GameMinDTO;
 import com.wydengames.wydenlist.entities.Game;
 import com.wydengames.wydenlist.repositories.GameRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,5 +20,13 @@ public class GameService {
 
     public List<GameMinDTO> findAll(){
         return gameRepository.findAll().stream().map(GameMinDTO::new).toList();
+    }
+
+    @Transactional
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Recurso nao encontrado")
+        );
+        return new GameDTO(result);
     }
 }
