1862224324c43ce88a585a39d08e40fda5996792
danieloliveeira <danieldoskil@gmail.com>
2025-08-24 13:12:31 -0300

    feat(GameListController): Adiciona novo endpoit de busca de jogos em listas com id

--- src/main/java/com/wydengames/wydenlist/controllers/GameListController.java
+++ src/main/java/com/wydengames/wydenlist/controllers/GameListController.java
@@ -8,6 +8,7 @@ import com.wydengames.wydenlist.services.GameListService;
 import com.wydengames.wydenlist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,11 +21,17 @@ public class GameListController {
     @Autowired
     private GameListService gameListService;
 
+    @Autowired
+    private GameService gameService;
+
     @GetMapping
     public List<GameListDTO> findAll(){
         return gameListService.findAll();
     }
 
-
+    @GetMapping(value = "/{listId}/games")
+    public List<GameMinDTO> findByList(@PathVariable("listId")Long listId){
+        return gameService.findByList(listId);
+    }
 
 }
