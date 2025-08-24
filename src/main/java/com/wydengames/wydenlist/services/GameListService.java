130062559b1c4e9ef5c6930168469d3a40e937ce
danieloliveeira <danieldoskil@gmail.com>
2025-08-24 20:08:23 -0300

    feat(GameListService): Adiciona novo metodo para mover itens(jogos) dentro da lista

--- src/main/java/com/wydengames/wydenlist/services/GameListService.java
+++ src/main/java/com/wydengames/wydenlist/services/GameListService.java
@@ -2,7 +2,11 @@ package com.wydengames.wydenlist.services;
 
 
 import com.wydengames.wydenlist.dto.GameListDTO;
+import com.wydengames.wydenlist.dto.GameMinDTO;
+import com.wydengames.wydenlist.projections.GameMinProjection;
 import com.wydengames.wydenlist.repositories.GameListRepository;
+import com.wydengames.wydenlist.repositories.GameRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +16,32 @@ import java.util.List;
 public class GameListService {
 
     @Autowired
+    private GameRepository gameRepository;
+
+    @Autowired
     private GameListRepository gameListRepository;
 
+    @Transactional
     public List<GameListDTO> findAll(){
         return gameListRepository.findAll().stream().map(GameListDTO::new).toList();
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex){
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+//        int min = sourceIndex < destinationIndex ? sourceIndex: destinationIndex;
+//        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+        int min = Math.min(sourceIndex, destinationIndex);
+        int max = Math.max(sourceIndex, destinationIndex);
+
+        for (int i = min; i <= max; i++){
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
+
     }
 
 }
