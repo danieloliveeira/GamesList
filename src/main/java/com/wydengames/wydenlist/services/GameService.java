c82a40f69221778fb397ce50ebb20e74179afe71
danieloliveeira <danieldoskil@gmail.com>
2025-08-24 01:53:58 -0300

    feat: Adiciona a camada de service

--- src/main/java/com/wydengames/wydenlist/services/GameService.java
+++ src/main/java/com/wydengames/wydenlist/services/GameService.java
@@ -1,4 +1,20 @@
 package com.wydengames.wydenlist.services;
 
+import com.wydengames.wydenlist.dto.GameMinDTO;
+import com.wydengames.wydenlist.entities.Game;
+import com.wydengames.wydenlist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll(){
+        return gameRepository.findAll().stream().map(GameMinDTO::new).toList();
+    }
 }
