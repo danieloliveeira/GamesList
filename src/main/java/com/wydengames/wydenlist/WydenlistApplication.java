a4d559b7a55fcd87e7def861e22fb0aff5208f77
danieloliveeira <danieldoskil@gmail.com>
2025-08-23 23:45:20 -0300

    feat: Configura estrutura inicial do projeto com Spring

---
+++ src/main/java/com/wydengames/wydenlist/WydenlistApplication.java
@@ -0,0 +1,13 @@
+package com.wydengames.wydenlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WydenlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WydenlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wydengames/wydenlist/WydenlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wydengames.wydenlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WydenlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
